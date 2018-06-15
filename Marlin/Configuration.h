2a53643050428a791e6120e7eaf48548d3507ce2
churristian8 Win <churristian8@gmail.com>
2018-06-15 19:07:46 +0200

    test

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1681,7 +1681,6 @@
  */
 //#define FILAMENT_WIDTH_SENSOR
 
-#define DEFAULT_NOMINAL_FILAMENT_DIA 3.00   // (mm) Diameter of the filament generally used (3.0 or 1.75mm), also used in the slicer. Used to validate sensor reading.
 #define DEFAULT_NOMINAL_FILAMENT_DIA 1.75   // (mm) Diameter of the filament generally used (3.0 or 1.75mm), also used in the slicer. Used to validate sensor reading.
 
 #if ENABLED(FILAMENT_WIDTH_SENSOR)
